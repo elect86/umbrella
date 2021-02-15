540b2e8f4defe4e01069ae4c0b2a8e42e94a0340
Giuseppe Barbieri <elect86@gmail.com>
2021-02-15 20:01:19 +0100

    stuck

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,6 +1,21 @@
 
 dependencyResolutionManagement.repositories.gradlePluginPortal()
 
+//pluginManagement {
+//    resolutionStrategy {
+//        eachPlugin {
+//            if(requested.id.id == "sciJava.catalogs")
+//                useModule("sciJava.catalogs:sciJava-catalogs:0.0.1")
+//        }
+//    }
+////    repositories {
+////        gradlePluginPortal()
+////        maven("https://jitpack.io")
+////    }
+//}
+
+//plugins { id("sciJava.catalogs") }
+
 rootProject.name = "build-logic"
 
 include("commons")
